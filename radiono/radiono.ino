1d53d90595988a153c82208d0ecc84b6d23a5559
Eldon R. Brown - WA0UWH <eldonb46@gmail.com>
2014-08-09 15:49:54 -0700

    DA - Update Rev Code

--- radiono/radiono.ino
+++ radiono/radiono.ino
@@ -24,7 +24,7 @@
 
 //#define RADIONO_VERSION "0.4"
 #define RADIONO_VERSION "0.4.erb" // Modifications by: Eldon R. Brown - WA0UWH
-#define INC_REV "CK"              // Incremental Rev Code
+#define INC_REV "DA"              // Incremental Rev Code
 
 
 /*
