c82aa06f86dff82d5f511aef44a5ab3452d951d7
Eldon R. Brown - WA0UWH <eldonb46@gmail.com>
2014-09-03 10:18:25 -0700

    FF - Updated Macro and Beacon Code, README.md Clean up

--- radiono/radiono.ino
+++ radiono/radiono.ino
@@ -42,7 +42,7 @@ void setup(); // # A Hack, An Arduino IED Compiler Preprocessor Fix
 
 //#define RADIONO_VERSION "0.4"
 #define RADIONO_VERSION "0.4.erb" // Modifications by: Eldon R. Brown - WA0UWH
-#define INC_REV "FE"              // Incremental Rev Code
+#define INC_REV "FF"              // Incremental Rev Code
 
 
 /*
