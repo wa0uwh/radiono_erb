8742850ff28532c1d2e14819ab86edc8994aa625
Eldon R. Brown - WA0UWH <eldonb46@gmail.com>
2014-09-06 17:28:35 -0700

    FL - Update Rev Code, and some simple cleanup

--- radiono/radiono.ino
+++ radiono/radiono.ino
@@ -44,7 +44,7 @@ void setup(); // # A Hack, An Arduino IED Compiler Preprocessor Fix
 
 //#define RADIONO_VERSION "0.4"
 #define RADIONO_VERSION "0.4.erb" // Modifications by: Eldon R. Brown - WA0UWH
-#define INC_REV "FK_01"              // Incremental Rev Code
+#define INC_REV "FL"              // Incremental Rev Code
 
 
 /*
