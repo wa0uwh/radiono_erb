c56cb5063fa82751d7a4b233e73c5e937f43738b
Eldon R. Brown - WA0UWH <eldonb46@gmail.com>
2014-10-18 11:53:23 -0700

    ERB_II - CleanUp

--- radiono/Rf386.cpp
+++ radiono/Rf386.cpp
@@ -1,6 +1,24 @@
 //Rf386.cpp
 // Support for RF386 Filter Selection
 
+/*
+ * Copyright (C) 2014, by Author: Eldon R. Brown (ERB) - WA0UWH
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <Arduino.h>
 #include "A1Main.h"
 #include "debug.h"
